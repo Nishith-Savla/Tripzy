dd03648dc3c6bc85f1c109c9a7f40cd027646595
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2023-04-01 17:26:00 +0530

    created model for enrolled trips

---
+++ backend/models/enrolled-trips-model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const EnrolledTripsSchema = new mongoose.Schema({
+	user_id: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	trip_id: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Trip",
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: () => new Date(),
+	},
+});
+
+modules.exports = mongoose.model("EnrolledTrips", EnrolledTripsSchema);
