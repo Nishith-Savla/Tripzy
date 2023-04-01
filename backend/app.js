14ddf70baa4486483833c78d15fd9c5a1cf433c4
Varshil-Shah <varshilashah09@gmail.com>
2023-04-01 13:02:44 +0530

    Implement global error handler

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const errorController = require("./controllers/error-controller");
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,12 @@ app.use(cors());
 app.options("*", cors());
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+// global error handler
+app.use(errorController);
 
 module.exports = app;
