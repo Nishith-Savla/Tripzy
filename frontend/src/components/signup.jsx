18f8bf7e153292222dfd99908b93150b0ac82884
Aayush Patel <rudaniaayush1@gmail.com>
2023-04-01 13:36:03 +0530

    Add auth page

---
+++ frontend/src/components/signup.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import "../css/signin.css"
+import "../css/style.css"
+import "../css/util.css"
+import img1 from "../assets/images/img1.jpeg"
+import img2 from "../assets/images/img2.jpg" 
+import img6 from "../assets/images/img6.jpg"
+import trpizyLogo from "../assets/logo/tripzy.png"
+import searchImage from "../assets/icons/search.png"
+
+
+function Signup(props) {
+  return (
+    <section id="login-page">
+  <header className="d-flex align-items-center g-3">
+    <img src={trpizyLogo} width="60px" alt="" />
+    <h1 className="secondary-font page-title larger-2">Tripzy</h1>
+  </header>
+  <div className="d-flex justify-content-between p-5 container">
+    <div className="fb-48 d-flex justify-content-center mt-5">
+      {/* The carousel part */}
+      <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item">
+            <img src={img1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item active">
+            <img src={img2} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={img6} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="vertical-line" />
+    <div className="fb-48 d-flex justify-content-center">
+      {/* The Form Part */}
+      <div className="input-container d-flex dir-col mt-5 align-items-center g-3">
+        <h3 className="text-center primary-font larger p-0 m-0 dark-purple">
+          Sign in
+        </h3>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button className="btn btn-primary">Sign in</button>
+        <p className="large primary-font">---------- or ----------</p>
+        {/* or signin with google */}
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-google d-flex align-items-center g-2">
+            <span>Sign in with Google</span>
+            <img src={searchImage} width="30px" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default Signup
