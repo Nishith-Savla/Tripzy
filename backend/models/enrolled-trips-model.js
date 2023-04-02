527fbdccde593b78cff1fcd1f2d468581b431a41
Varshil-Shah <varshilashah09@gmail.com>
2023-04-02 11:42:47 +0530

    Resolve enrolled error

--- backend/models/enrolled-trips-model.js
+++ backend/models/enrolled-trips-model.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 const EnrolledTripsSchema = new mongoose.Schema({
-	userId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "User",
-		required: true,
-	},
-	tripId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Trip",
-		required: true,
-	},
-	date: {
-		type: Date,
-		default: () => new Date(),
-	},
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  tripId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Trip",
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 module.exports = mongoose.model("EnrolledTrips", EnrolledTripsSchema);
