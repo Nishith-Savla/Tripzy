c0c1395fbab8d4dbf6f8c7c54d3d53992d95a635
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2023-04-02 11:07:57 +0530

    addcoin

--- backend/controllers/enrolled-trips-controller.js
+++ backend/controllers/enrolled-trips-controller.js
@@ -1,7 +1,7 @@
 const EnrolledTripsModel = require("../models/enrolled-trips-model");
 const AppError = require("../utils/app-error");
 const catchAsync = require("../utils/catch-async");
-const addCoins = require("../utils/addCoins");
+const { addCoins } = require("../utils/coins");
 
 exports.getAllEnrolledTrips = catchAsync(async (req, res, next) => {
 	const enrolled_trips = await EnrolledTripsModel.find({
