68e1ab4ac3cf8d71cd9a07dde04ec705571d738b
Aayush Patel <rudaniaayush1@gmail.com>
2023-04-02 10:33:58 +0530

    Pulled all trips in frontend

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,28 +1,83 @@
 import React from "react";
 import Card from "./card";
 import "../css/dashboard.css";
+import { getEnrolledTrips, getTrips } from "../api/trips";
+import { useQuery } from "@tanstack/react-query";
+import { formatDate } from "../utils";
 
 function Dashboard() {
+	const allTripsQuery = useQuery({
+		queryKey: ["trips"],
+		queryFn: getTrips,
+	});
+
+	const enrolledTripsQuery = useQuery({
+		queryKey: ["trips", localStorage.getItem("access_token")],
+		enabled: allTripsQuery.data !== undefined,
+		queryFn: getEnrolledTrips,
+	});
+
+	if (allTripsQuery.isLoading && allTripsQuery.fetchStatus !== "idle") {
+		return <div>Loading...</div>;
+	}
+
+	console.log(allTripsQuery.data);
+	const { data: allTrips } = allTripsQuery.data;
+	const enrolledTripsQueryData = enrolledTripsQuery.data;
+	let enrolledTrips = null;
+	if (enrolledTripsQueryData) {
+		enrolledTrips = enrolledTripsQueryData.data;
+	}
+
 	return (
 		<div id="dashboard">
-			<h1>My trips</h1>
-
-			<div
-				style={{
-					display: "grid",
-					gridTemplateColumns: "repeat(4, 1fr)",
-				}}
-			>
-				<Card
-					title="Span"
-					createdBy="Aayush Patel"
-					startDate="23"
-					endDate="24"
-					memberCount={12}
-					coverImage={[
-						"https://travellersworldwide.com/wp-content/uploads/2022/06/shutterstock_712575202.jpg.webp",
-					]}
-				/>
+			<div>
+				<h1>All trips</h1>
+				<div
+					style={{
+						display: "grid",
+						gridTemplateColumns: "repeat(3, 1fr)",
+					}}
+				>
+					{enrolledTrips?.length !== 0
+						? enrolledTrips?.map((trip) => {
+								return (
+									<Card
+										key={trip._id}
+										title={trip.title}
+										createdBy={trip.createdBy}
+										startDate={formatDate(trip.startDate)}
+										endDate={formatDate(trip.endDate)}
+										memberCount={trip.memberCount}
+										coverImage={trip.coverImage}
+									/>
+								);
+						  })
+						: null}
+				</div>
+			</div>
+			<div>
+				<h1>All trips</h1>
+				<div
+					style={{
+						display: "grid",
+						gridTemplateColumns: "repeat(3, 1fr)",
+					}}
+				>
+					{allTrips.map((trip) => {
+						return (
+							<Card
+								key={trip._id}
+								title={trip.title}
+								createdBy={trip.createdBy}
+								startDate={formatDate(trip.startDate)}
+								endDate={formatDate(trip.endDate)}
+								memberCount={trip.memberCount}
+								coverImage={trip.coverImage}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
